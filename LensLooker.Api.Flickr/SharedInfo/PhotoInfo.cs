c123f097326fc4ca7a74d9f59fe77125046dfcb7
Juliet Boyd <5061312+julietrb1@users.noreply.github.com>
2022-06-13 23:31:48 +1000

    Make final aperture decimal optional

--- LensLooker.Api.Flickr/SharedInfo/PhotoInfo.cs
+++ LensLooker.Api.Flickr/SharedInfo/PhotoInfo.cs
@@ -4,9 +4,9 @@ namespace LensLooker.Api.Flickr.SharedInfo;
 
 public static class PhotoInfo
 {
-    private const string CanonLensSpec = @"\s?\d{1,3}(-\d{1,3})?\s?mm\sf/\d(\.\d)?(-\d(\.\d))?";
+    private const string CanonLensSpec = @"\s?\d{1,3}(-\d{1,3})?\s?mm\sf/\d(\.\d)?(-\d(\.\d)?)?";
     private const string CanonLensPost = @"(\s|$)";
-    private const string SonyLensSpec = @"\s?\d{1,3}(-\d{1,3})?\s?mm\sF\d(\.\d)?(-\d(\.\d))?";
+    private const string SonyLensSpec = @"\s?\d{1,3}(-\d{1,3})?\s?mm\sF\d(\.\d)?(-\d(\.\d)?)?";
     private const string SonyGLensPost = @"\sG";
     private const string SonyLensPost = @"($|\s[A-FH-Y])";
     private const string SonyZaLensPost = @"\sZA";
