3e55c7797a9f7a2e912b86e04951aca73b935630
Juliet Boyd <5061312+julietrb1@users.noreply.github.com>
2022-06-13 22:54:47 +1000

    Order lens families

--- LensLooker.Site/Data/PhotoService.cs
+++ LensLooker.Site/Data/PhotoService.cs
@@ -59,7 +59,9 @@ internal class PhotoService : IPhotoService
             .Include(l => l.Photos)
             .ToList();
 
-        return lenses.GroupBy(l => $"{l.LensFamily!.CameraBrand.Name} {l.LensFamily.Name}");
+        return lenses
+            .GroupBy(l => $"{l.LensFamily!.CameraBrand.Name} {l.LensFamily.Name}")
+            .OrderBy(g => g.Key);
     }
 
     private async Task<PhotosResult> GetPhotosFromDatabase(string? lensName, int pageNumber, int pageSize)
