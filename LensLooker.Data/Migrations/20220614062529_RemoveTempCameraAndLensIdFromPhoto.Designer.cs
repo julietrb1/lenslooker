50798cf18c1bc80e53fd316a659c2547ee27e96b
Juliet Boyd <5061312+julietrb1@users.noreply.github.com>
2022-06-14 16:25:46 +1000

    Remove temp camera and lens ID from Photo

--- LensLooker.Data/Migrations/20220614062529_RemoveTempCameraAndLensIdFromPhoto.Designer.cs
+++ LensLooker.Data/Migrations/20220614062529_RemoveTempCameraAndLensIdFromPhoto.Designer.cs
@@ -4,6 +4,7 @@ using LensLooker.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LensLooker.Data.Migrations
 {
     [DbContext(typeof(LensLookerContext))]
-    partial class LensLookerContextModelSnapshot : ModelSnapshot
+    [Migration("20220614062529_RemoveTempCameraAndLensIdFromPhoto")]
+    partial class RemoveTempCameraAndLensIdFromPhoto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LensLooker.Data/Migrations/20220614062529_RemoveTempCameraAndLensIdFromPhoto.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LensLooker.Data.Migrations
+{
+    public partial class RemoveTempCameraAndLensIdFromPhoto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TempCameraId",
+                table: "Photos");
+
+            migrationBuilder.DropColumn(
+                name: "TempLensId",
+                table: "Photos");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TempCameraId",
+                table: "Photos",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TempLensId",
+                table: "Photos",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
