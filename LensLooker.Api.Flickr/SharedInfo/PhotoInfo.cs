2e0c3ea7f961a7a8bbc04baf04bab7f4f9553278
Juliet Boyd <5061312+julietrb1@users.noreply.github.com>
2024-05-31 13:53:36 +1000

    Pass matchTimeout to regex patterns

--- LensLooker.Api.Flickr/SharedInfo/PhotoInfo.cs
+++ LensLooker.Api.Flickr/SharedInfo/PhotoInfo.cs
@@ -14,25 +14,25 @@ public static class PhotoInfo
 
     public static readonly FrozenDictionary<Regex, string> CanonLensFamilyRegexes = new Dictionary<Regex, string>
     {
-        [new Regex($"^EF{CanonLensSpec}{CanonLensPost}")] = "EF",
-        [new($"^EF{CanonLensSpec}L{CanonLensPost}")] = "EF L",
-        [new($"^EF-M{CanonLensSpec}{CanonLensPost}")] = "EF-M",
-        [new($"^EF-S{CanonLensSpec}{CanonLensPost}")] = "EF-S",
-        [new($"^RF{CanonLensSpec}{CanonLensPost}")] = "RF",
-        [new($"^RF{CanonLensSpec}L{CanonLensPost}")] = "RF L"
+        [new Regex($"^EF{CanonLensSpec}{CanonLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "EF",
+        [new($"^EF{CanonLensSpec}L{CanonLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "EF L",
+        [new($"^EF-M{CanonLensSpec}{CanonLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "EF-M",
+        [new($"^EF-S{CanonLensSpec}{CanonLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "EF-S",
+        [new($"^RF{CanonLensSpec}{CanonLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "RF",
+        [new($"^RF{CanonLensSpec}L{CanonLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "RF L"
     }.ToFrozenDictionary();
 
     public static readonly FrozenDictionary<Regex, string> SonyLensFamilyRegexes = new Dictionary<Regex, string>
     {
-        [new Regex($"^DT{SonyLensSpec}{SonyLensPost}")] = "DT",
-        [new Regex($"^DT{SonyLensSpec}{SonyGLensPost}")] = "DT G",
-        [new Regex($"^DT{SonyLensSpec}{SonyZaLensPost}")] = "DT ZA",
-        [new Regex($"^E{SonyLensSpec}{SonyLensPost}")] = "E",
-        [new Regex($"^FE{SonyLensSpec}{SonyLensPost}")] = "FE",
-        [new Regex($"^FE{SonyLensSpec}{SonyGLensPost}")] = "FE G",
-        [new Regex($"^FE{SonyLensSpec}{SonyZaLensPost}")] = "FE ZA",
-        [new Regex($"^{SonyLensSpec}{SonyLensPost}")] = "SAL",
-        [new Regex($"^{SonyLensSpec}{SonyGLensPost}")] = "SAL G",
-        [new Regex($"^{SonyLensSpec}{SonyZaLensPost}")] = "SAL ZA"
+        [new Regex($"^DT{SonyLensSpec}{SonyLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "DT",
+        [new Regex($"^DT{SonyLensSpec}{SonyGLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "DT G",
+        [new Regex($"^DT{SonyLensSpec}{SonyZaLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "DT ZA",
+        [new Regex($"^E{SonyLensSpec}{SonyLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "E",
+        [new Regex($"^FE{SonyLensSpec}{SonyLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "FE",
+        [new Regex($"^FE{SonyLensSpec}{SonyGLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "FE G",
+        [new Regex($"^FE{SonyLensSpec}{SonyZaLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "FE ZA",
+        [new Regex($"^{SonyLensSpec}{SonyLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "SAL",
+        [new Regex($"^{SonyLensSpec}{SonyGLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "SAL G",
+        [new Regex($"^{SonyLensSpec}{SonyZaLensPost}", RegexOptions.None, TimeSpan.FromMilliseconds(100))] = "SAL ZA"
     }.ToFrozenDictionary();
 }
